844a5f7587961052a0e5a62babe341ba9781069e
Gabriel Plinio <gabriel.machado.tblh@gmail.com>
2025-04-13 11:16:49 -0300

    adicionado função extraiParagrafo, limpaPalavras e limpaPalavras

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 const fs = require('fs');
+const trataErros = require('./erros/funcoesErro');
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-fs.readFile(link, 'utf-8', (err , data) => {
-    if (err) throw err;
-    console.log(data);
+fs.readFile(link, 'utf-8', (erro , data) => {
+    try{
+        if (erro) throw erro
+        contaPalavras(data)
+    }
+    catch(erro){
+        trataErros(erro)    
+    }
 })
 
-console.log(link)
+function contaPalavras(texto){
+    const paragrafos = extraiParagrafos(texto)
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if(!paragrafo) return []
+        return verificaPalavrasDuplicadas(paragrafo)
+    })
+    console.log(contagem)
+}
+
+function extraiParagrafos(texto){
+    return texto.toLowerCase().split('\n')
+}
+
+function limpaPalavras(palavra){
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ')
+    const resultado = {};
+
+    listaPalavras.forEach(palavra => {
+        if(palavra.length >= 3){
+            const palavraLimpa = limpaPalavras(palavra)
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+        }
+    })
+    return resultado
+}
